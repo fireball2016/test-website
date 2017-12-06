a0c9a0005abeb2dee8cd630725cd6ad4db4d2cac
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-06 15:18:02 -0800

    commit

---
+++ server/server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const port = process.env.PORT || 3000;
+const website = require('../website/index.html');
+
+var app = express();
+
+app.get('/', (req, res) => {
+  res.sendFile(website)
+  })
+});
+
+app.listen(port, () => {
+  console.log(`Server is up on port ${port}`);
+});
