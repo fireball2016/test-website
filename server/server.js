7e925f963edb4fef0c45fce6ff4523be7873f9af
meatball <meatball@meatballs-MacBook-Pro.local>
2017-12-06 15:37:03 -0800

    damn

--- server/server.js
+++ server/server.js
@@ -3,11 +3,7 @@ const port = process.env.PORT || 3000;
 
 var app = express();
 
-app.use('/static', express.static('website'));
-app.get('/static', (req, res) => {
-  res.sendFile('index.html')
-})
-
+app.use(express.static(__dirname + '/website'));
 
 app.listen(port, () => {
   console.log(`Server is up on port ${port}`);
